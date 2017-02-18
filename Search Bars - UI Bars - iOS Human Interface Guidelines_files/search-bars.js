308b5de753df15fcf7277867773367cf93605fea
SeungHye Lee i <sugarui@naver.com>
2017-02-18 18:08:57 +0900

    Search Bars 꼭지, 변경 전 원본 파일 추가

---
+++ Search Bars - UI Bars - iOS Human Interface Guidelines_files/search-bars.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	$('.search-bar-elements-gallery').bxSlider({
+		adaptiveHeight: false,
+		mode: 'fade',
+		pagerCustom: '#bxlabels-search-bar-elements',
+		controls: false
+	});
+
+});
